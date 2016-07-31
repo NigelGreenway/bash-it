f65657d1eec767773004dc9f456956cfe8d285dc
Nigel Greenway <github@futurepixels.co.uk>
2016-07-31 18:55:46 +0100

    Add ability to configure line breaks in a theme

--- themes/bakke/bakke.theme.bash
+++ themes/bakke/bakke.theme.bash
@@ -12,11 +12,29 @@ GIT_THEME_PROMPT_SUFFIX="${green}|"
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
+RVM_THEME_PROMPT_PRE_FIXED_LINE_BREAK="\n"
+function disable_prefixed_line_break() {
+	RVM_THEME_PROMPT_PRE_FIXED_LINE_BREAK=""
+}
+
+RVM_THEME_PROMPT_APPENDED_LINE_BREAK=" "
+function enable_appended_line_break() {
+	RVM_THEME_PROMPT_APPENDED_LINE_BREAK="\n"
+}
+
+if [ "$BASH_IT_THEME_ENABLE_PREPENDED_LINE_BREAK" = false ]; then
+	disable_prefixed_line_break
+fi
+
+if [ "$BASH_IT_THEME_ENABLE_APPENDED_LINE_BREAK" = true ]; then
+	enable_appended_line_break
+fi
+
 function prompt_command() {
-    #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    #PS1="\n${purple}\h: ${reset_color} ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
-    #PS1="\n${cyan}\h: ${reset_color} ${yellow}\w\n${red}$(scm_char)${red}$(scm_prompt_info) ${green}→${reset_color} "
-    PS1="\n${cyan}\h: ${reset_color} ${yellow}\w ${green}$(scm_prompt_info)\n${reset_color}→ "
+    #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${RVM_THEME_PROMPT_APPENDED_LINE_BREAK}${reset_color} "
+    #PS1="${RVM_THEME_PROMPT_PRE_FIXED_LINE_BREAK}${purple}\h: ${reset_color} ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${RVM_THEME_PROMPT_APPENDED_LINE_BREAK}${reset_color} "
+    #PS1="${RVM_THEME_PROMPT_PRE_FIXED_LINE_BREAK}${cyan}\h: ${reset_color} ${yellow}\w\n${red}$(scm_char)${red}$(scm_prompt_info) ${green}${RVM_THEME_PROMPT_APPENDED_LINE_BREAK}→${reset_color} "
+    PS1="${RVM_THEME_PROMPT_PRE_FIXED_LINE_BREAK}${cyan}\h: ${reset_color} ${yellow}\w ${green}$(scm_prompt_info)${RVM_THEME_PROMPT_APPENDED_LINE_BREAK}${reset_color}→ "
 }
 
 safe_append_prompt_command prompt_command
